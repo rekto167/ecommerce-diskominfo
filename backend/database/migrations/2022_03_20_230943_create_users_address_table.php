0805a5f711652b8b8d0b9b8286f406858ed80dbf
mirebus <ekysr99@gmail.com>
2022-03-21 06:36:51 +0700

    creating migration

---
+++ backend/database/migrations/2022_03_20_230943_create_users_address_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_address', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->longText('address')->default('');
+            $table->string('city')->default('');
+            $table->string('province')->default('');
+            $table->string('country')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_address');
+    }
+}
