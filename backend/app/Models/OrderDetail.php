98b0eafed8f5115d04af8fd0ea46cb0822cba45d
mirebus <ekysr99@gmail.com>
2022-03-21 18:19:04 +0700

    order

--- backend/app/Models/OrderDetail.php
+++ backend/app/Models/OrderDetail.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cart;
+use App\Models\Payment;
+use App\Models\UserAddress;
 
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'total_price',
+        'payment_id',
+        'address_id'
+    ];
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public function address()
+    {
+        return $this->belongsTo(UserAddress::class);
+    }
 }
--- backend/app/Models/OrderItem.php
+++ backend/app/Models/OrderItem.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\OrderDetail;
+use App\Models\Product;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'orders_details_id',
+        'product_id',
+    ];
+
+    public function orderDetail()
+    {
+        return $this->belongsTo(OrderDetail::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
