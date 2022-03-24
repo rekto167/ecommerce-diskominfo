2c32a10f6d42644a1c61aac9ff57017407108f85
mirebus <ekysr99@gmail.com>
2022-03-24 09:50:07 +0700

    api login, register

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'username' => 'required|string|unique:users',
+                'email' => 'email|required|unique:users',
+                'phone' => 'required|string',
+                'password' => 'required'
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            if($user){
+                return ResponseFormatter::error([
+                    'message' => 'Your account has been registered',
+                ], 'Unauthorized', 401);
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'User Registered');
+        } catch (Exception $e) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $e
+            ], 'Register Failed', 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+            if(!$user) {
+                return ResponseFormatter::error([
+                    'message' => 'User not registered',
+                ], 'Unauthorized', 401);
+            }
+            if(!Hash::check($request->password, $user->password)){
+                return ResponseFormatter::error([
+                    'message' => 'Password incorrect',
+                ], 'Unauthorized', 401);
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'type_token' => 'Bearer',
+                'message' => 'Login Successfully',
+                'user' => $user
+            ], 'Authorized');
+        } catch (\Exception $e) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $e
+            ], 'Server Error', 500);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,9 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 
-Route::get('products', [ProductController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('products', [ProductController::class, 'index']);
+});
